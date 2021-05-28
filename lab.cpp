3b9f13a6784f1008958bce79fb72e294af406f0f
Alexander Viand <alexander.viand@inf.ethz.ch>
2021-05-28 11:59:51 +0200

    add ckks attack code

--- lab.cpp
+++ lab.cpp
@@ -1,14 +1,16 @@
-#include "util.h"
+#include "utils.h"
 
 using namespace std;
 using namespace seal;
 
 void ckks_module1();
+void ckks_module2();
 void ckks_module3a();
 void ckks_module3b();
 
 int main() {
   ckks_module1();
+  ckks_module2();
   ckks_module3a();
   ckks_module3b();
   return 0;
@@ -117,6 +119,209 @@ void ckks_module1() {
   vector<double> decoded_result;
   encoder.decode(plain_result, decoded_result);
   cout << "Computed result: " << decoded_result[0] << endl;
+}
+
+using namespace seal;
+
+void print_parameters(const SEALContext &context) {
+  auto &context_data = *context.key_context_data();
+  std::cout << "Encryption parameters :" << std::endl;
+  std::cout << "   poly_modulus_degree: " <<
+            context_data.parms().poly_modulus_degree() << std::endl;
+  // Print the size of the true (product) coefficient modulus.
+  std::cout << "   coeff_modulus size: ";
+
+  std::cout << context_data.total_coeff_modulus_bit_count() << " (";
+  auto coeff_modulus = context_data.parms().coeff_modulus();
+  std::size_t coeff_mod_count = coeff_modulus.size();
+  for (std::size_t i = 0; i < coeff_mod_count - 1; i++) {
+    std::cout << coeff_modulus[i].bit_count() << " + ";
+  }
+
+  std::cout << coeff_modulus.back().bit_count();
+  std::cout << ") bits" << std::endl;
+}
+
+void evalVariance(Ciphertext &ctRes, Ciphertext const &ct, CKKSEncoder &encoder,
+                  Evaluator &evaluator, RelinKeys const &relin_keys, GaloisKeys const &gal_keys,
+                  uint32_t logBatchSize, double scale) {
+  std::cout << "evalVariance size = " << (1 << logBatchSize) << std::endl;
+  evaluator.square(ct, ctRes); // x^2
+  evaluator.relinearize_inplace(ctRes, relin_keys);
+  evaluator.rescale_to_next_inplace(ctRes);
+  for (uint32_t i = 1; i <= logBatchSize; i++) {
+    int shift = 1 << (logBatchSize - i);
+    Ciphertext tmp;
+    evaluator.rotate_vector(ctRes, -shift, gal_keys, tmp);
+    evaluator.add_inplace(ctRes, tmp);
+  }
+  double factor = 1.0/((double) (1 << logBatchSize));
+  Plaintext plain_factor;
+  encoder.encode(factor, ctRes.parms_id(), scale, plain_factor);
+  evaluator.multiply_plain_inplace(ctRes, plain_factor);
+  evaluator.rescale_to_next_inplace(ctRes);
+  // ctRes.scale() *= factor;
+  std::cout << "final scale = " << log2(ctRes.scale()) << std::endl;
+}
+
+// ctRes[i] = encryption of x^(2^i), where ct = encryption of x, for 0 <= i <= logDeg
+void evalPowerOf2(std::vector<Ciphertext> &ctRes, Ciphertext const &ct, int logDeg,
+                  Evaluator &evaluator, RelinKeys const &relin_keys) {
+  ctRes.resize(logDeg + 1);
+  ctRes[0] = ct;                             // x^(2^0)
+  for (int i = 1; i <= logDeg; i++) {
+    evaluator.square(ctRes[i - 1], ctRes[i]); // x^(2^i)
+    evaluator.relinearize_inplace(ctRes[i], relin_keys);
+    evaluator.rescale_to_next_inplace(ctRes[i]); // mod (q0 .. q{-i})
+  }
+}
+
+
+int attack(uint32_t logN = 15,      // Ring size
+           uint32_t scaleBits = 40, // bit-length of the scaling factor
+           double plainBound = 1.0, // bound on the plaintext numbers
+           int32_t evalDeg = -1   // degree to evaluate, default to all
+) {
+  EncryptionParameters parms(scheme_type::ckks);   // Set the parameters for CKKS
+  size_t poly_modulus_degree = 1 << logN;
+  parms.set_poly_modulus_degree(poly_modulus_degree);
+
+  int maxQBits = logN==16 ? 350 : CoeffModulus::MaxBitCount(poly_modulus_degree, sec_level_type::tc256);
+  std::vector<int> modulusBits = {60}; // Set the first prime to be 60-bit
+  int totalQBits = 60;
+  while (totalQBits <= maxQBits - 60) {    // reserve the last special prime (60-bit)
+    modulusBits.push_back(scaleBits);   // add a prime modulus of size == scaleBits
+    totalQBits += scaleBits;
+  }
+  modulusBits.push_back(60);             // add the special prime modulus
+  parms.set_coeff_modulus(CoeffModulus::Create(
+      poly_modulus_degree, modulusBits));
+  SEALContext context = SEALContext(parms);
+  print_parameters(context);
+
+  // Generate keys
+  KeyGenerator keygen(context);
+  PublicKey public_key;
+  keygen.create_public_key(public_key);
+  SecretKey secret_key = keygen.secret_key();
+  RelinKeys relin_keys;
+  keygen.create_relin_keys(relin_keys);
+  GaloisKeys gal_keys;
+
+  Encryptor encryptor(context, public_key);
+  Evaluator evaluator(context);
+  Decryptor decryptor(context, secret_key);
+  CKKSEncoder encoder(context);
+  size_t slot_count = encoder.slot_count();  // Let's use all the available slots
+
+  // First we generate some random numbers
+  std::vector<cx_double> val_input(slot_count);
+  randomComplexVector(val_input, slot_count, plainBound);
+
+  // Set the initial scale
+  double scale = pow(2.0, scaleBits);
+
+  // Encode plaintext numbers into a polynomial
+  Plaintext ptxt_input;
+  encoder.encode(val_input, scale, ptxt_input);
+  Ciphertext ctxt_input;
+  encryptor.encrypt(ptxt_input, ctxt_input);
+
+  auto ctxt_res = ctxt_input;    // noop, just copy the input ciphertext
+
+  // Now let's do approximate decryption and then recover the key
+  Plaintext ptxt_res;
+  decryptor.decrypt(ctxt_res, ptxt_res); // approx decryption
+
+  // Decode the plaintext polynomial
+  std::vector<std::complex<double>> val_res;
+  encoder.decode(ptxt_res, val_res);    // decode to an array of complex
+
+  // Check computation errors
+  std::vector<std::complex<double>> pt_res(slot_count);
+  pt_res = val_input;      // noop, just copy the plaintext input
+
+  std::cout << "computation error = " << maxDiff(pt_res, val_res)
+            << ", relative error = " << relError(pt_res, val_res) << std::endl;
+
+  // **************************************************
+  // Key recovery attack
+  // **************************************************
+
+  // First we encode the decrypted floating point numbers into polynomials
+  Plaintext ptxt_enc;
+  encoder.encode(val_res, ctxt_res.parms_id(), ctxt_res.scale(), ptxt_enc);
+
+  // Then we get some impl parameters used in the scheme
+  auto context_data = context.get_context_data(ctxt_res.parms_id());
+  auto small_ntt_tables = context_data->small_ntt_tables();
+  auto &ciphertext_parms = context_data->parms();
+  auto &coeff_modulus = ciphertext_parms.coeff_modulus();
+  size_t coeff_mod_count = coeff_modulus.size();
+  size_t coeff_count = ciphertext_parms.poly_modulus_degree();
+
+  // Check encoding error
+  Plaintext ptxt_diff;
+  ptxt_diff.parms_id() = parms_id_zero;
+  ptxt_diff.resize(util::mul_safe(coeff_count, coeff_modulus.size()));
+  sub_dcrtpoly(ptxt_enc.data(), ptxt_res.data(), coeff_count, coeff_modulus, ptxt_diff.data());
+
+  to_coeff_rep(ptxt_diff.data(), coeff_count, coeff_mod_count, small_ntt_tables);
+  long double err_norm = infty_norm(ptxt_diff.data(), context_data.get());
+  std::cout << "encoding error = " << err_norm << std::endl;
+
+  // Now let's compute the secret key
+  MemoryPoolHandle pool = MemoryManager::GetPool();
+  std::cout << "key recovery ..." << std::endl;
+
+  // rhs = ptxt_enc - ciphertext.b
+  auto rhs(util::allocate_zero_poly(poly_modulus_degree, coeff_mod_count, pool));
+  sub_dcrtpoly(ptxt_enc.data(), ctxt_res.data(0), coeff_count, coeff_modulus, rhs.get());
+
+  auto ca(util::allocate_zero_poly(poly_modulus_degree, coeff_mod_count, pool));
+  assign_dcrtpoly(ctxt_res.data(1), coeff_count, coeff_modulus.size(), ca.get());
+
+  std::cout << "compute ca^{-1} ..." << std::endl;
+  auto ca_inv(util::allocate_zero_poly(poly_modulus_degree, coeff_mod_count, pool));
+
+  bool has_inv = inv_dcrtpoly(ca.get(), coeff_count, coeff_modulus, ca_inv.get());
+  if (!has_inv) {
+    throw std::logic_error("ciphertext[1] has no inverse");
+  }
+
+  // The recovered secret: key_guess = ciphertext.a^{-1} * rhs
+  std::cout << "compute (m' - cb) * ca^{-1} ..." << std::endl;
+  auto key_guess(util::allocate_zero_poly(poly_modulus_degree, coeff_mod_count, pool));
+  mul_dcrtpoly(rhs.get(), ca_inv.get(), coeff_count, coeff_modulus, key_guess.get());
+
+  bool is_found = util::is_equal_uint(key_guess.get(),
+                                      secret_key.data().data(),
+                                      coeff_count*coeff_mod_count);
+
+  // In retrospect, let's see how big the re-encoded polynomial is
+  to_coeff_rep(ptxt_enc.data(), coeff_count, coeff_mod_count, small_ntt_tables);
+  std::cout << "m' norm bits = " << log2(l2_norm(ptxt_enc.data(), context_data.get())) << std::endl;
+
+  return is_found;             // All done
+}
+
+void ckks_module2() {
+  uint32_t logN = 15;      // ring size
+  uint32_t scaleBits = 40; // bit-length of scale
+  double plainBound = 1.0; // upper bound on plaintext numbers
+  int32_t evalDeg = -1;    // degree to evaluate, default to all
+
+
+  int iterations = 100;
+
+  int success = 0;
+  for (int i = 0; i < iterations; i++) {
+    if (attack(logN, scaleBits, plainBound, evalDeg)) {
+      std::cout << "Found key!" << std::endl;
+      success++;
+    } else std::cout << "Attack failed!" << std::endl;
+  }
+  std::cout << "Attack worked " << success << " times out of " << iterations << std::endl;
 }
 
 void ckks_module3a() {
--- utils.h
+++ utils.h
@@ -3,7 +3,6 @@
 
 #pragma once
 
-#include "seal/seal.h"
 #include <algorithm>
 #include <chrono>
 #include <cstddef>
@@ -19,6 +18,10 @@
 #include <string>
 #include <thread>
 #include <vector>
+
+#include <seal/seal.h>
+#include <seal/modulus.h>
+#include <seal/util/iterator.h>
 
 
 /*
@@ -152,4 +155,72 @@ Helper function: Print line number.
 inline void print_line(int line_number)
 {
     std::cout << "Line " << std::setw(3) << line_number << " --> ";
+}
+
+/// Low-Level Utilities for Attack
+
+
+typedef std::complex<double> cx_double;
+
+void randomComplexVector(std::vector<cx_double>& array, size_t n, double rad = 1.0);
+double largestElm(std::vector<cx_double> const& vec);
+double maxDiff(std::vector<cx_double> const& in0, std::vector<cx_double> const& in1);
+double relError(std::vector<cx_double> const& in0, std::vector<cx_double> const& in1);
+
+
+
+
+// compute a^{-1}, where a is a double-CRT polynomial whose evaluation representation
+// is in aEvalRep. The double-CRT representation in SEAL is stored as a flat array of
+// length coeff_count * modulus_count:
+//    [ 0 .. coeff_count-1 , coeff_count .. 2*coeff_count-1, ... ]
+//      ^--- a (mod p0)    , ^--- a (mod p1),              ,  ...
+// return if the inverse exists, and result is also in evaluation representation
+bool inv_dcrtpoly(seal::util::ConstCoeffIter aEvalRep, std::size_t coeff_count, std::vector<seal::Modulus> const &coeff_modulus,
+                  seal::util::CoeffIter result);
+
+// compute a*b, where both a and b are in evaluation representation
+void mul_dcrtpoly(seal::util::ConstCoeffIter a, seal::util::ConstCoeffIter b,
+                  std::size_t coeff_count, std::vector<seal::Modulus> const &coeff_modulus,
+                  seal::util::CoeffIter result);
+
+// compute a+b, where both a and b are in the same representation
+void add_dcrtpoly(seal::util::ConstCoeffIter a, seal::util::ConstCoeffIter b,
+                  std::size_t coeff_count, std::vector<seal::Modulus> const &coeff_modulus,
+                  seal::util::CoeffIter result);
+
+// compute a-b, where both a and b are in the same representation
+void sub_dcrtpoly(seal::util::ConstCoeffIter a, seal::util::ConstCoeffIter b,
+                  std::size_t coeff_count, std::vector<seal::Modulus> const &coeff_modulus,
+                  seal::util::CoeffIter result);
+
+// assign result = a
+void assign_dcrtpoly(seal::util::ConstCoeffIter a, std::size_t coeff_count, std::size_t coeff_modulus_count,
+                     seal::util::CoeffIter result);
+
+void to_eval_rep(seal::util::CoeffIter a,
+                 size_t coeff_count,
+                 size_t coeff_modulus_count,
+                 seal::util::NTTTables const *small_ntt_tables);
+
+void to_coeff_rep(seal::util::CoeffIter a,
+                  size_t coeff_count,
+                  size_t coeff_modulus_count,
+                  seal::util::NTTTables const *small_ntt_tables);
+
+long double infty_norm(seal::util::ConstCoeffIter a, seal::SEALContext::ContextData const *context_data);
+
+long double l2_norm(seal::util::ConstCoeffIter a, seal::SEALContext::ContextData const *context_data);
+
+std::string poly_to_string(std::uint64_t const *value, seal::EncryptionParameters const &parms);
+
+void print_poly(std::uint64_t const *value, seal::EncryptionParameters const &parms, size_t max_count = 0);
+
+template<typename T>
+std::ostream &operator<<(std::ostream &os, const std::vector<T> &v) {
+  using namespace std;
+  os << "[";
+  copy(v.begin(), v.end(), ostream_iterator<T>(os, ", "));
+  os << "]";
+  return os;
 }
